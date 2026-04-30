7b26dc942a0f89435eecd4a6d74abed9ba401d7f
MiniXebec1 <magizhanskrp@gmail.com>
2026-04-30 20:01:11 +0800

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,23 @@
+var choice = prompt("Welcome to Area Calculator. \n2.Area of Triangle. \n3.Area of Circle. \n4.Area of Parallelogram");
+
+if (choice == '1') {
+    var 1 = prompt('Enter the Length')
+    var b = prompt('Enter the breadth')
+    var result = Number(1) * Number(b)
+    alert('The Area is' + result)
+}
+
+if (choice == '2') {
+    var h = prompt('Enter the height')
+    var b = prompt('Enter the base')
+    var result = Number(1) * Number(b) / 2
+    alert('The Area is' + result)
+}
+if (choice -- '4') {
+    var h = prompt('Enter the height')
+    var cb = prompt('Enter the corresponding base')
+    var result = Number(h) * Number(cb) * Number(r)
+    alert('The Area is ' + result)
+}
+
+
